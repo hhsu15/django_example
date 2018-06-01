8562648fae944411aa5dcde628693a23f701ea75
Hsinheng Hsu <jessicatung@Jessicas-Air.fios-router.home>
2018-05-31 21:44:23 -0400

    user login example

--- hello/urls.py
+++ hello/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path("",views.index, name='index'),
     path("<int:flight_id>", views.flight, name='flight'),
     path("<int:flight_id>/book", views.book, name='book'),
+    path("user_index", views.user_index, name='user_index'),
+    path("login", views.login_view, name='login'),
+    path("logout", views.logout_view, name='logout'),
+
 
 ]
